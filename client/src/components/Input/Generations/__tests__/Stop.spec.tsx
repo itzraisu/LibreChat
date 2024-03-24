e4fcab7a453dbc086c84dd4fccdcbe9a4c7462d7
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:19 -0300

    improve

--- client/src/components/Input/Generations/__tests__/Stop.spec.tsx
+++ client/src/components/Input/Generations/__tests__/Stop.spec.tsx
@@ -1,15 +1,10 @@
-import { render, fireEvent } from 'test/layout-test-utils';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Stop from '../Stop';
 
 describe('Stop', () => {
   it('should render the Stop button', () => {
-    const { getByText } = render(
-      <Stop
-        onClick={() => {
-          ('');
-        }}
-      />,
-    );
+    const { getByText } = render(<Stop />);
     expect(getByText('Stop')).toBeInTheDocument();
   });
 
@@ -19,4 +14,12 @@ describe('Stop', () => {
     fireEvent.click(getByText('Stop'));
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
+
+  it('should not call onClick when the button is disabled and clicked', () => {
+    const handleClick = jest.fn();
+    const { getByText } = render(<Stop onClick={handleClick} disabled={true} />);
+    fireEvent.click(getByText('Stop'));
+    expect(handleClick).toHaveBeenCalledTimes(0);
+  });
 });
+
