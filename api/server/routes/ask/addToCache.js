55bb2e1e7b7179aacdb844483798dbcadf4c0060
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:01 -0300

    improve

--- api/server/routes/ask/addToCache.js
+++ api/server/routes/ask/addToCache.js
@@ -2,61 +2,55 @@ const Keyv = require('keyv');
 const { KeyvFile } = require('keyv-file');
 const { logger } = require('~/config');
 
+const createConversation = async (conversationId, userMessage, responseMessage, endpointOption) => {
+  const conversationsCache = new Keyv({
+    store: new KeyvFile({ filename: './data/cache.json' }),
+    namespace: 'chatgpt', // should be 'bing' for bing/sydney
+  });
+
+  const getRole = (endpoint, options) => {
+    if (endpoint === 'openAI') {
+      return options?.chatGptLabel || 'ChatGPT';
+    } else if (endpoint === 'bingAI') {
+      return options?.jailbreak ? 'Sydney' : 'BingAI';
+    }
+  };
+
+  const conversation = await conversationsCache.get(conversationId);
+
+  if (!conversation) {
+    return conversationsCache.set(conversationId, {
+      messages: [
+        createMessage('User', userMessage),
+        createMessage(getRole(endpointOption.endpoint, endpointOption), responseMessage),
+      ],
+      createdAt: Date.now(),
+    });
+  }
+
+  conversation.messages.push(
+    createMessage('User', userMessage),
+    createMessage(getRole(endpointOption.endpoint, endpointOption), responseMessage)
+  );
+
+  await conversationsCache.set(conversationId, conversation);
+};
+
+const createMessage = (role, message) => ({
+  id: message.messageId,
+  parentMessageId: message.parentMessageId,
+  role,
+  message: message.text,
+});
+
 const addToCache = async ({ endpoint, endpointOption, userMessage, responseMessage }) => {
   try {
-    const conversationsCache = new Keyv({
-      store: new KeyvFile({ filename: './data/cache.json' }),
-      namespace: 'chatgpt', // should be 'bing' for bing/sydney
-    });
-
-    const {
-      conversationId,
-      messageId: userMessageId,
-      parentMessageId: userParentMessageId,
-      text: userText,
-    } = userMessage;
-    const {
-      messageId: responseMessageId,
-      parentMessageId: responseParentMessageId,
-      text: responseText,
-    } = responseMessage;
-
-    let conversation = await conversationsCache.get(conversationId);
-    // used to generate a title for the conversation if none exists
-    // let isNewConversation = false;
-    if (!conversation) {
-      conversation = {
-        messages: [],
-        createdAt: Date.now(),
-      };
-      // isNewConversation = true;
-    }
-
-    const roles = (options) => {
-      if (endpoint === 'openAI') {
-        return options?.chatGptLabel || 'ChatGPT';
-      } else if (endpoint === 'bingAI') {
-        return options?.jailbreak ? 'Sydney' : 'BingAI';
-      }
-    };
-
-    let _userMessage = {
-      id: userMessageId,
-      parentMessageId: userParentMessageId,
-      role: 'User',
-      message: userText,
-    };
-
-    let _responseMessage = {
-      id: responseMessageId,
-      parentMessageId: responseParentMessageId,
-      role: roles(endpointOption),
-      message: responseText,
-    };
-
-    conversation.messages.push(_userMessage, _responseMessage);
-
-    await conversationsCache.set(conversationId, conversation);
+    await createConversation(
+      userMessage.conversationId,
+      userMessage,
+      responseMessage,
+      endpointOption
+    );
   } catch (error) {
     logger.error('[addToCache] Error adding conversation to cache', error);
   }
