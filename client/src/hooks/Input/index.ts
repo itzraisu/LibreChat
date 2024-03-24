0c759fa9045578286f96f90cbf8e968515efce46
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:56:28 -0300

    improve

--- client/src/hooks/Input/index.ts
+++ client/src/hooks/Input/index.ts
@@ -1,5 +1,8 @@
+// hooks/index.js
+
 export { default as useUserKey } from './useUserKey';
 export { default as useDebounce } from './useDebounce';
 export { default as useTextarea } from './useTextarea';
 export { default as useRequiresKey } from './useRequiresKey';
 export { default as useMultipleKeys } from './useMultipleKeys';
+
