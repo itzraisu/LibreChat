1424bc039762fa936bda291a0f358bbe69c54283
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:31 -0300

    improve

--- api/test/jestSetup.js
+++ api/test/jestSetup.js
@@ -1,6 +1,15 @@
-// See .env.test.example for an example of the '.env.test' file.
+// Load environment variables from .env.test file
 require('dotenv').config({ path: './test/.env.test' });
 
-process.env.BAN_VIOLATIONS = 'true';
-process.env.BAN_DURATION = '7200000';
-process.env.BAN_INTERVAL = '20';
+// Set production-like environment variables for testing
+const environmentVariables = {
+  BAN_VIOLATIONS: 'true',
+  BAN_DURATION: '7200000',
+  BAN_INTERVAL: '20',
+};
+
+Object.keys(environmentVariables).forEach((key) => {
+  if (process.env[key] === undefined) {
+    process.env[key] = environmentVariables[key];
+  }
+});
