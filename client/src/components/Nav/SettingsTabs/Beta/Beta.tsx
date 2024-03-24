4bdb00efb8126a473ea3ee36b5f8ab5404c4c648
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:56:00 -0300

    improve

--- client/src/components/Nav/SettingsTabs/Beta/Beta.tsx
+++ client/src/components/Nav/SettingsTabs/Beta/Beta.tsx
@@ -4,23 +4,21 @@ import { SettingsTabValues } from 'librechat-data-provider';
 import LaTeXParsing from './LaTeXParsing';
 import ModularChat from './ModularChat';
 
-function Beta() {
+type BetaProps = {};
+
+const Beta: React.FC<BetaProps> = () => {
   return (
-    <Tabs.Content
-      value={SettingsTabValues.BETA}
-      role="tabpanel"
-      className="w-full md:min-h-[300px]"
-    >
+    <Tabs.Content value={SettingsTabValues.BETA} className="w-full md:min-h-[300px]">
       <div className="flex flex-col gap-3 text-sm text-gray-600 dark:text-gray-50">
-        <div className="border-b pb-3 last-of-type:border-b-0 dark:border-gray-700">
+        <div title="Modular Chat" key="modular-chat" className="border-b pb-3 last-of-type:border-b-0 dark:border-gray-700">
           <ModularChat />
         </div>
-        <div className="border-b pb-3 last-of-type:border-b-0 dark:border-gray-700">
+        <div title="LaTeX Parsing" key="latex-parsing" className="border-b pb-3 last-of-type:border-b-0 dark:border-gray-700">
           <LaTeXParsing />
         </div>
       </div>
     </Tabs.Content>
   );
-}
+};
 
 export default memo(Beta);
