aec6024e92840c46aaa3235507baa0b632f97095
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:15 -0300

    improve

--- api/utils/logger.js
+++ api/utils/logger.js
@@ -1,12 +1,25 @@
 const winston = require('winston');
 
+// Set up a custom logging format
+const myFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.printf(info => {
+    return `${info.timestamp} ${info.level}: ${info.message}`;
+  })
+);
+
+// Create the logger with the specified settings
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+  format: myFormat,
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'login-logs.log' }),
-  ],
+    new winston.transports.File({ filename: 'login-logs.log' })
+  ]
 });
 
+// Ensure that the logger is always able to log, even if there's an error
+logger.exitOnError = false;
+
+// Export the logger as a module
 module.exports = logger;
