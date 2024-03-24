4cc213d331589ff7e4f05e5812cec152d9ddc0b7
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:54:47 -0300

    improve

--- api/app/clients/tools/util/addOpenAPISpecs.spec.js
+++ api/app/clients/tools/util/addOpenAPISpecs.spec.js
@@ -1,11 +1,16 @@
 const { addOpenAPISpecs, transformSpec } = require('./addOpenAPISpecs');
 const { loadSpecs } = require('./loadSpecs');
 const { createOpenAPIPlugin } = require('../dynamic/OpenAPIPlugin');
+const { describe, it, beforeEach, expect } = require('jest');
 
 jest.mock('./loadSpecs');
 jest.mock('../dynamic/OpenAPIPlugin');
 
 describe('transformSpec', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('should transform input spec to a desired format', () => {
     const input = {
       name_for_human: 'Human Name',
@@ -44,9 +49,34 @@ describe('transformSpec', () => {
 
     expect(transformSpec(input)).toEqual(expectedOutput);
   });
+
+  it('should include authConfig if provided', () => {
+    const input = {
+      name_for_human: 'Human Name',
+      name_for_model: 'Model Name',
+      description_for_human: 'Human Description',
+      logo_url: 'https://example.com/logo.png',
+      authConfig: ['auth1', 'auth2'],
+    };
+
+    const expectedOutput = {
+      name: 'Human Name',
+      pluginKey: 'Model Name',
+      description: 'Human Description',
+      icon: 'https://example.com/logo.png',
+      isAuthRequired: 'false',
+      authConfig: ['auth1', 'auth2'],
+    };
+
+    expect(transformSpec(input)).toEqual(expectedOutput);
+  });
 });
 
 describe('addOpenAPISpecs', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('should add specs to available tools', async () => {
     const availableTools = ['Tool1', 'Tool2'];
     const specs = [
@@ -58,19 +88,4 @@ describe('addOpenAPISpecs', () => {
       },
     ];
 
-    loadSpecs.mockResolvedValue(specs);
-    createOpenAPIPlugin.mockReturnValue('Plugin');
-
-    const result = await addOpenAPISpecs(availableTools);
-    expect(result).toEqual([...specs.map(transformSpec), ...availableTools]);
-  });
-
-  it('should return available tools if specs loading fails', async () => {
-    const availableTools = ['Tool1', 'Tool2'];
-
-    loadSpecs.mockRejectedValue(new Error('Failed to load specs'));
-
-    const result = await addOpenAPISpecs(availableTools);
-    expect(result).toEqual(availableTools);
-  });
-});
+    loadSpec
