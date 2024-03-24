7d4da9370233b0a84a7e29f6ea540eac4b3f650b
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:56:27 -0300

    improve

--- client/src/utils/files.ts
+++ client/src/utils/files.ts
@@ -21,61 +21,22 @@ const spreadsheet = {
 const codeFile = {
   paths: CodePaths,
   fill: '#FF6E3C',
-  // TODO: make this dynamic to the language
   title: 'Code',
 };
 
 export const fileTypes = {
-  /* Category matches */
   file: {
     paths: FilePaths,
     fill: '#0000FF',
     title: 'File',
   },
   text: textDocument,
-  // application:,
-
-  /* Partial matches */
   csv: spreadsheet,
   pdf: textDocument,
   'text/x-': codeFile,
-
-  /* Exact matches */
-  // 'application/json':,
-  // 'text/html':,
-  // 'text/css':,
-  // image,
 };
 
-// export const getFileType = (type = '') => {
-//   let fileType = fileTypes.file;
-//   const exactMatch = fileTypes[type];
-//   const partialMatch = !exactMatch && partialTypes.find((type) => type.includes(type));
-//   const category = (!partialMatch && (type.split('/')[0] ?? 'text') || 'text');
-
-//   if (exactMatch) {
-//     fileType = exactMatch;
-//   } else if (partialMatch) {
-//     fileType = fileTypes[partialMatch];
-//   } else if (fileTypes[category]) {
-//     fileType = fileTypes[category];
-//   }
-
-//   if (!fileType) {
-//     fileType = fileTypes.file;
-//   }
-
-//   return fileType;
-// };
-
-export const getFileType = (
-  type = '',
-): {
-  paths: React.FC;
-  fill: string;
-  title: string;
-} => {
-  // Direct match check
+export const getFileType = (type = '') => {
   if (fileTypes[type]) {
     return fileTypes[type];
   }
@@ -84,19 +45,16 @@ export const getFileType = (
     return spreadsheet;
   }
 
-  // Partial match check
   const partialMatch = partialTypes.find((partial) => type.includes(partial));
   if (partialMatch && fileTypes[partialMatch]) {
     return fileTypes[partialMatch];
   }
 
-  // Category check
   const category = type.split('/')[0] || 'text';
   if (fileTypes[category]) {
     return fileTypes[category];
   }
 
-  // Default file type
   return fileTypes.file;
 };
 
