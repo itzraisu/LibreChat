cb0fea47600a84ccd39cd8df86ef0f0af537257e
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:53 -0300

    improve

--- api/utils/debug.js
+++ api/utils/debug.js
@@ -5,52 +5,57 @@ const levels = {
   HIGH: 3,
 };
 
-let level = levels.HIGH;
+let currentLevel = levels.HIGH;
+
+const log = {
+  setLevel: (l) => (currentLevel = l),
+
+  parameterLogGroup: (parameters) => {
+        if (levels.HIGH > currentLevel) {
+          return;
+        }
+        console.groupCollapsed(`Parameters:`);
+        parameters.forEach((p) => console.log(`${p.name}:`, p.value));
+        console.groupEnd();
+      },
+
+      functionName: (name) => {
+        if (levels.MEDIUM > currentLevel) {
+          return;
+        }
+        console.log(`\nEXECUTING: ${name}\n`);
+      },
+
+      flow: (flow) => {
+        if (levels.LOW > currentLevel) {
+          return;
+        }
+        console.log(`\n\n\nBEGIN FLOW: ${flow}\n\n\n`);
+      },
+
+      variable: ({ name, value }) => {
+        if (levels.HIGH > currentLevel) {
+          return;
+        }
+        console.groupCollapsed(`VARIABLE ${name}:`);
+        console.log(value);
+        console.groupEnd();
+      },
+
+      request: (req, res, next) => {
+        if (levels.HIGH > currentLevel) {
+          return next();
+        }
+        console.log('Hit URL', req.url);
+        console.groupCollapsed('Request details:');
+        console.log('Query:', req.query);
+        console.log('Body:', req.body);
+        console.groupEnd();
+        return next();
+      },
+};
 
 module.exports = {
   levels,
-  setLevel: (l) => (level = l),
-  log: {
-    parameters: (parameters) => {
-      if (levels.HIGH > level) {
-        return;
-      }
-      console.group();
-      parameters.forEach((p) => console.log(`${p.name}:`, p.value));
-      console.groupEnd();
-    },
-    functionName: (name) => {
-      if (levels.MEDIUM > level) {
-        return;
-      }
-      console.log(`\nEXECUTING: ${name}\n`);
-    },
-    flow: (flow) => {
-      if (levels.LOW > level) {
-        return;
-      }
-      console.log(`\n\n\nBEGIN FLOW: ${flow}\n\n\n`);
-    },
-    variable: ({ name, value }) => {
-      if (levels.HIGH > level) {
-        return;
-      }
-      console.group();
-      console.group();
-      console.log(`VARIABLE ${name}:`, value);
-      console.groupEnd();
-      console.groupEnd();
-    },
-    request: () => (req, res, next) => {
-      if (levels.HIGH > level) {
-        return next();
-      }
-      console.log('Hit URL', req.url, 'with following:');
-      console.group();
-      console.log('Query:', req.query);
-      console.log('Body:', req.body);
-      console.groupEnd();
-      return next();
-    },
-  },
+  ...log,
 };
