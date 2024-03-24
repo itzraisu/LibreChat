8f9c97e6c092928eef2b2563cdbd7b8ac44bfe1a
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:56:13 -0300

    improve

--- api/cache/getLogStores.js
+++ api/cache/getLogStores.js
@@ -5,7 +5,7 @@ const { math, isEnabled } = require('~/server/utils');
 const keyvRedis = require('./keyvRedis');
 const keyvMongo = require('./keyvMongo');
 
-const { BAN_DURATION, USE_REDIS } = process.env ?? {};
+const { BAN_DURATION = '7200000', USE_REDIS = 'false' } = process.env;
 
 const duration = math(BAN_DURATION, 7200000);
 
@@ -14,33 +14,32 @@ const createViolationInstance = (namespace) => {
   return new Keyv(config);
 };
 
-// Serve cache from memory so no need to clear it on startup/exit
 const pending_req = isEnabled(USE_REDIS)
   ? new Keyv({ store: keyvRedis })
   : new Keyv({ namespace: 'pending_req' });
 
-const config = isEnabled(USE_REDIS)
-  ? new Keyv({ store: keyvRedis })
-  : new Keyv({ namespace: CacheKeys.CONFIG_STORE });
+const configStore = isEnabled(USE_REDIS)
+  ? new Keyv({ store: keyvRedis, ttl: 1800000 })
+  : new Keyv({ namespace: CacheKeys.CONFIG_STORE, ttl: 1800000 });
 
-const tokenConfig = isEnabled(USE_REDIS) // ttl: 30 minutes
+const tokenConfig = isEnabled(USE_REDIS)
   ? new Keyv({ store: keyvRedis, ttl: 1800000 })
   : new Keyv({ namespace: CacheKeys.TOKEN_CONFIG, ttl: 1800000 });
 
-const genTitle = isEnabled(USE_REDIS) // ttl: 2 minutes
+const genTitle = isEnabled(USE_REDIS)
   ? new Keyv({ store: keyvRedis, ttl: 120000 })
   : new Keyv({ namespace: CacheKeys.GEN_TITLE, ttl: 120000 });
 
-const modelQueries = isEnabled(process.env.USE_REDIS)
-  ? new Keyv({ store: keyvRedis })
-  : new Keyv({ namespace: CacheKeys.MODEL_QUERIES });
+const modelQueries = isEnabled(USE_REDIS)
+  ? new Keyv({ store: keyvRedis, ttl: 1800000 })
+  : new Keyv({ namespace: CacheKeys.MODEL_QUERIES, ttl: 1800000 });
 
 const abortKeys = isEnabled(USE_REDIS)
-  ? new Keyv({ store: keyvRedis })
+  ? new Keyv({ store: keyvRedis, ttl: 600000 })
   : new Keyv({ namespace: CacheKeys.ABORT_KEYS, ttl: 600000 });
 
 const namespaces = {
-  [CacheKeys.CONFIG_STORE]: config,
+  [CacheKeys.CONFIG_STORE]: configStore,
   pending_req,
   ban: new Keyv({ store: keyvMongo, namespace: 'bans', ttl: duration }),
   general: new Keyv({ store: logFile, namespace: 'violations' }),
@@ -69,10 +68,5 @@ const namespaces = {
  * @throws Will throw an error if an invalid key is passed.
  */
 const getLogStores = (key) => {
-  if (!key || !namespaces[key]) {
-    throw new Error(`Invalid store key: ${key}`);
-  }
-  return namespaces[key];
-};
-
-module.exports = getLogStores;
+  if (typeof key !== 'string') {
+    throw new Error(`Invalid
