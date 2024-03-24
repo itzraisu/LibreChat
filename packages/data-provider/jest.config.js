11392a1079ca9d8b931fbfaf17d1129b89374ca0
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:56:01 -0300

    improve

--- packages/data-provider/jest.config.js
+++ packages/data-provider/jest.config.js
@@ -1,11 +1,28 @@
 module.exports = {
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/node_modules/'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
-  coverageReporters: ['text', 'cobertura'],
+  roots: ['<rootDir>/src'],
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir}/node_modules/',
+    '!<rootDir}/dist/',
+    '!<rootDir}/src/index.tsx',
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+  ],
+  coverageReporters: [
+    'text',
+    'cobertura',
+  ],
   testResultsProcessor: 'jest-junit',
   moduleNameMapper: {
     '^@src/(.*)$': '<rootDir>/src/$1',
   },
+  restoreMocks: true,
   // coverageThreshold: {
   //   global: {
   //     statements: 58,
@@ -14,5 +31,4 @@ module.exports = {
   //     lines: 57,
   //   },
   // },
-  restoreMocks: true,
 };
