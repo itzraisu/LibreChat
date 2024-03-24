46912ca461b9b36f707d584a575e03e912f3a072
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:54:55 -0300

    improve

--- client/src/components/Messages/Content/index.ts
+++ client/src/components/Messages/Content/index.ts
@@ -1,3 +1,15 @@
-export { default as SubRow } from './SubRow';
-export { default as Plugin } from './Plugin';
-export { default as MessageContent } from './MessageContent';
+// import individual components
+import SubRow from './SubRow';
+import Plugin from './Plugin';
+import MessageContent from './MessageContent';
+
+// create a new index.js file and re-export the components
+export { SubRow };
+export { Plugin };
+export { MessageContent };
+
+// or, alternatively, you can re-export them using object shorthand syntax
+// const SubRow = './SubRow';
+// const Plugin = './Plugin';
+// const MessageContent = './MessageContent';
+// export { SubRow, Plugin, MessageContent };
