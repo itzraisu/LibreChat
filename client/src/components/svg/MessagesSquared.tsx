901a63aee6984e3b23f9be373f563a17d8b81433
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:56:08 -0300

    improve

--- client/src/components/svg/MessagesSquared.tsx
+++ client/src/components/svg/MessagesSquared.tsx
@@ -1,6 +1,9 @@
+import React, { forwardRef } from 'react';
+import classNames from 'classnames';
+import { IconProps } from './types';
 import { cn } from '~/utils/';
 
-export default function MessagesSquared({ className }: { className?: string }) {
+const MessagesSquare = forwardRef<SVGSVGElement, IconProps>(({ className, ...props }, ref) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -12,10 +15,16 @@ export default function MessagesSquared({ className }: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className={cn(className ?? '', 'lucide lucide-messages-square')}
+      className={classNames(className, 'lucide lucide-messages-square')}
+      ref={ref}
+      {...props}
     >
       <path d="M14 9a2 2 0 0 1-2 2H6l-4 4V4c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v5Z" />
       <path d="M18 9h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1" />
     </svg>
   );
-}
+});
+
+MessagesSquare.displayName = 'MessagesSquare';
+
+export default MessagesSquare;
