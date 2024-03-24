7b90abc92edbb4748b04688de63c48a69793dd4b
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:47 -0300

    improve

--- client/src/components/ui/QuestionMark.tsx
+++ client/src/components/ui/QuestionMark.tsx
@@ -1,15 +1,19 @@
 import { cn } from '~/utils';
-export const QuestionMark = ({ className = '' }) => {
+
+const QuestionMark = ({ className = '', size = '3.5', ...rest }) => {
   return (
     <span>
       <div
         className={cn(
-          'border-token-border-medium text-token-text-tertiary ml-2 flex h-3.5 w-3.5 cursor-default items-center justify-center rounded-full border text-[0.5rem] font-medium leading-none',
+          `border-token-border-medium text-token-text-tertiary ml-2 flex h-${size} w-${size} cursor-default items-center justify-center rounded-full border text-[0.5rem] font-medium leading-none`,
           className,
         )}
+        {...rest}
       >
         ?
       </div>
     </span>
   );
 };
+
+export default QuestionMark;
