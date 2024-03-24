45545572cbc0584ef482fabea2c28c497d00e932
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:59 -0300

    improve

--- client/src/components/Plugins/Store/index.ts
+++ client/src/components/Plugins/Store/index.ts
@@ -1,6 +1,22 @@
+// plugins.js
+
 export { default as PluginStoreDialog } from './PluginStoreDialog';
 export { default as PluginStoreItem } from './PluginStoreItem';
 export { default as PluginPagination } from './PluginPagination';
 export { default as PluginStoreLinkButton } from './PluginStoreLinkButton';
 export { default as PluginAuthForm } from './PluginAuthForm';
 export { default as PluginTooltip } from './PluginTooltip';
+
+// index.js
+
+import * as plugins from './plugins';
+
+export default plugins;
+
+
+import plugins from 'path/to/plugins';
+
+// Use the plugins like this:
+const PluginStoreDialog = plugins.PluginStoreDialog;
+const PluginStoreItem = plugins.PluginStoreItem;
+// ...and so on.
