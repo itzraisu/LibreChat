d800ea8bf0ab617fb521ce5068a7a332263322a2
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:21 -0300

    improve

--- client/src/components/Endpoints/Settings/MultiView/GoogleSettings.tsx
+++ client/src/components/Endpoints/Settings/MultiView/GoogleSettings.tsx
@@ -1,25 +1,30 @@
-import Settings from '../Google';
+import Settings from '../Google/Settings';
 import Examples from '../Examples';
 import { useSetIndexOptions } from '~/hooks';
 import { useChatContext } from '~/Providers';
 
 export default function GoogleView({ conversation, models, isPreset = false }) {
   const { optionSettings } = useChatContext();
-  const { setOption, setExample, addExample, removeExample } = useSetIndexOptions(
-    isPreset ? conversation : null,
-  );
+  const {
+    setOption,
+    setExample,
+    addExample,
+    removeExample,
+  } = useSetIndexOptions(isPreset ? conversation : null);
+
   if (!conversation) {
     return null;
   }
 
   const { examples, model } = conversation;
-  const isGenerativeModel = model?.toLowerCase()?.includes('gemini');
-  const isChatModel = !isGenerativeModel && model?.toLowerCase()?.includes('chat');
-  const isTextModel = !isGenerativeModel && !isChatModel && /code|text/.test(model ?? '');
+  const isGenerativeModel = /gemini/.test(model?.toLowerCase() || '');
+  const isChatModel = !isGenerativeModel && /chat/.test(model?.toLowerCase() || '');
+  const isTextModel = !isGenerativeModel && !isChatModel && /code|text/.test(model || '');
   const { showExamples } = optionSettings;
+
   return showExamples && isChatModel && !isTextModel ? (
     <Examples
-      examples={examples ?? [{ input: { content: '' }, output: { content: '' } }]}
+      examples={examples || [{ input: { content: '' }, output: { content: '' } }]}
       setExample={setExample}
       addExample={addExample}
       removeExample={removeExample}
@@ -28,3 +33,5 @@ export default function GoogleView({ conversation, models, isPreset = false }) {
     <Settings conversation={conversation} setOption={setOption} models={models} />
   );
 }
+
+
