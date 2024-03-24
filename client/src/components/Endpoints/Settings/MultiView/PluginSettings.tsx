04b4334ec487e0839333b4af02df93f66be72c11
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:04 -0300

    improve

--- client/src/components/Endpoints/Settings/MultiView/PluginSettings.tsx
+++ client/src/components/Endpoints/Settings/MultiView/PluginSettings.tsx
@@ -1,18 +1,29 @@
-import Settings from '../Plugins';
-import AgentSettings from '../AgentSettings';
+import { Settings, AgentSettings } from '../Plugins';
 import { useSetIndexOptions } from '~/hooks';
 import { useChatContext } from '~/Providers';
 
-export default function PluginsView({ conversation, models, isPreset = false }) {
+export default function PluginsView({ conversation = {}, models = [], isPreset = false }) {
   const { showAgentSettings } = useChatContext();
   const { setOption, setAgentOption } = useSetIndexOptions(isPreset ? conversation : null);
+
   if (!conversation) {
     return null;
   }
 
-  return showAgentSettings ? (
-    <AgentSettings conversation={conversation} setOption={setAgentOption} models={models} />
-  ) : (
-    <Settings conversation={conversation} setOption={setOption} models={models} />
+  return (
+    showAgentSettings ? (
+      <AgentSettings
+        conversation={conversation}
+        setOption={setAgentOption}
+        models={models}
+      />
+    ) : (
+      <Settings
+        conversation={conversation}
+        setOption={setOption}
+        models={models}
+      />
+    )
   );
 }
+
