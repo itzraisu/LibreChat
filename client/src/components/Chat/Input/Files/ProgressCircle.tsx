4365f6b6b19d1cd627e463cd0ebcbc9a45e81608
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:55 -0300

    improve

--- client/src/components/Chat/Input/Files/ProgressCircle.tsx
+++ client/src/components/Chat/Input/Files/ProgressCircle.tsx
@@ -1,15 +1,26 @@
+import React, { CSSProperties } from 'react';
+
 export default function ProgressCircle({
-  circumference,
-  offset,
-  circleCSSProperties,
+  circumference = 360,
+  offset = 0,
+  circleCSSProperties = { strokeDashoffset: 0 },
 }: {
-  circumference: number;
-  offset: number;
-  circleCSSProperties: React.CSSProperties;
+  circumference?: number;
+  offset?: number;
+  circleCSSProperties?: CSSProperties;
 }) {
   return (
-    <div className="absolute inset-0 flex items-center justify-center bg-black/5 text-white">
-      <svg width="120" height="120" viewBox="0 0 120 120" className="h-6 w-6">
+    <div
+      className="absolute inset-0 flex items-center justify-center bg-black/5 text-white"
+      role="progressbar"
+      aria-valuenow={100 - (offset / circumference) * 100}
+    >
+      <svg
+        width="120"
+        height="120"
+        viewBox="0 0 120 120"
+        className="h-6 w-6"
+      >
         <circle
           className="origin-[50%_50%] -rotate-90 stroke-gray-400"
           strokeWidth="10"
@@ -17,6 +28,7 @@ export default function ProgressCircle({
           r="55"
           cx="60"
           cy="60"
+          strokeLinecap="butt"
         />
         <circle
           className="origin-[50%_50%] -rotate-90 transition-[stroke-dashoffset]"
@@ -28,9 +40,5 @@ export default function ProgressCircle({
           r="55"
           cx="60"
           cy="60"
-          style={circleCSSProperties}
-        />
-      </svg>
-    </div>
-  );
-}
+          strokeLinecap="butt"
+          transform-origin="50% 50%
