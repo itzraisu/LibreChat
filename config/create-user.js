c8640aedb394c8b96817307e97d325c3c14fc6a1
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:56:31 -0300

    improve

--- config/create-user.js
+++ config/create-user.js
@@ -1,9 +1,19 @@
 const path = require('path');
-require('module-alias')({ base: path.resolve(__dirname, '..', 'api') });
-const { registerUser } = require('~/server/services/AuthService');
+const readline = require('readline');
+const { check, validationResult } = require('express-validator');
+const { registerUser } = require('../server/services/AuthService');
 const { askQuestion, silentExit } = require('./helpers');
-const User = require('~/models/User');
+const User = require('../models/User');
 const connect = require('./connect');
+const { moduleAlias, dotenv } = require('../package.json').dependencies;
+
+if (!moduleAlias || !dotenv) {
+  console.error('Error: The "module-alias" and/or "dotenv" packages are missing.');
+  process.exit(1);
+}
+
+require('module-alias')({ base: path.resolve(__dirname, '..', 'api') });
+dotenv.config();
 
 (async () => {
   await connect();
@@ -14,102 +24,109 @@ const connect = require('./connect');
   console.purple('--------------------------');
   console.purple('Create a new user account!');
   console.purple('--------------------------');
-  // If we don't have enough arguments, show the help menu
-  if (process.argv.length < 5) {
-    console.orange('Usage: npm run create-user <email> <name> <username>');
-    console.orange('Note: if you do not pass in the arguments, you will be prompted for them.');
-    console.orange(
-      'If you really need to pass in the password, you can do so as the 4th argument (not recommended for security).',
-    );
-    console.purple('--------------------------');
-  }
 
   /**
    * Set up the variables we need and get the arguments if they were passed in
    */
-  let email = '';
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const args = process.argv.slice(2);
+  const email = args[0];
+  const name = args[1];
+  const username = args[2];
   let password = '';
-  let name = '';
-  let username = '';
-  // If we have the right number of arguments, lets use them
-  if (process.argv.length >= 4) {
-    email = process.argv[2];
-    name = process.argv[3];
 
-    if (process.argv.length >= 5) {
-      username = process.argv[4];
+  if (args.length === 4) {
+    password = args[3];
+  } else {
+    rl.question('Password: (leave blank, to generate one)', (input) => {
+      if (input) {
+        password = input;
+      }
+      rl.close();
+    });
+  }
+
+  rl.on('close', async () => {
+    if (!email || !name || !username) {
+      console.orange('Usage: npm run create-user <email> <name> <username>');
+      console.orange('Note: if you do not pass in the arguments, you will be prompted for them.');
+      console.orange(
+        'If you really need to pass in the password, you can do so as the 4th argument (not recommended for security).',
+      );
+      console.purple('--------------------------');
+      silentExit(1);
     }
-    if (process.argv.length >= 6) {
-      console.red('Warning: password passed in as argument, this is not secure!');
-      password = process.argv[5];
+
+    /**
+     * Validate the email
+     */
+    const emailValidationRules = [
+      check('email').isEmail().withMessage('Error: Invalid email address!'),
+    ];
+
+    const validatedEmail = await new Promise((resolve) => {
+      const errors = validationResult(rl);
+      if (!errors.isEmpty()) {
+        console.red(errors.array()[0].msg);
+        resolve(null);
+      }
+      resolve(email);
+    });
+
+    if (!validatedEmail) {
+      silentExit(1);
     }
-  }
 
-  /**
-   * If we don't have the right number of arguments, lets prompt the user for them
-   */
-  if (!email) {
-    email = await askQuestion('Email:');
-  }
-  // Validate the email
-  if (!email.includes('@')) {
-    console.red('Error: Invalid email address!');
-    silentExit(1);
-  }
+    /**
+     * Set the default name and username if they are not provided
+     */
+    const defaultName = validatedEmail.split('@')[0];
+    const finalName = name || defaultName;
+    const finalUsername = username || defaultName;
 
-  const defaultName = email.split('@')[0];
-  if (!name) {
-    name = await askQuestion('Name: (default is: ' + defaultName + ')');
-    if (!name) {
-      name = defaultName;
+    /**
+     * Check if the user already exists
+     */
+    const userExists = await User.findOne({ $or: [{ email: validatedEmail }, { username: finalUsername }] });
+
+    if (userExists) {
+      console.red('Error: A user with that email or username already exists!');
+      silentExit(1);
     }
-  }
-  if (!username) {
-    username = await askQuestion('Username: (default is: ' + defaultName + ')');
-    if (!username) {
-      username = defaultName;
+
+    /**
+     * Create the user
+     */
+    const user = { email: validatedEmail, password, name: finalName, username: finalUsername, confirm_password: password };
+    let result;
+    try {
+      result = await registerUser(user);
+    } catch (error) {
+      console.red('Error: ' + error.message);
+      silentExit(1);
     }
-  }
-  if (!password) {
-    password = await askQuestion('Password: (leave blank, to generate one)');
-    if (!password) {
-      // Make it a random password, length 18
-      password = Math.random().toString(36).slice(-18);
-      console.orange('Your password is: ' + password);
+
+    /**
+     * Check the result
+     */
+    if (result.status !== 200) {
+      console.red('Error: ' + result.message);
+      silentExit(1);
     }
-  }
 
-  // Validate the user doesn't already exist
-  const userExists = await User.findOne({ $or: [{ email }, { username }] });
-  if (userExists) {
-    console.red('Error: A user with that email or username already exists!');
-    silentExit(1);
-  }
-
-  /**
-   * Now that we have all the variables we need, lets create the user
-   */
-  const user = { email, password, name, username, confirm_password: password };
-  let result;
-  try {
-    result = await registerUser(user);
-  } catch (error) {
-    console.red('Error: ' + error.message);
-    silentExit(1);
-  }
-
-  // Check the result
-  if (result.status !== 200) {
-    console.red('Error: ' + result.message);
-    silentExit(1);
-  }
-
-  // Done!
-  const userCreated = await User.findOne({ $or: [{ email }, { username }] });
-  if (userCreated) {
-    console.green('User created successfully!');
-    silentExit(0);
-  }
+    /**
+     * Done!
+     */
+    const userCreated = await User.findOne({ $or: [{ email: validatedEmail }, { username: finalUsername }] });
+    if (userCreated) {
+      console.green('User created successfully!');
+      silentExit(0);
+    }
+  });
 })();
 
 process.on('uncaughtException', (err) => {
