5ffcc09aa746e5e9611a0db635c48a9a0114fa6a
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:48 -0300

    improve

--- client/src/components/Auth/index.ts
+++ client/src/components/Auth/index.ts
@@ -3,3 +3,18 @@ export { default as Registration } from './Registration';
 export { default as ResetPassword } from './ResetPassword';
 export { default as ApiErrorWatcher } from './ApiErrorWatcher';
 export { default as RequestPasswordReset } from './RequestPasswordReset';
+
+// A better version using a single export statement
+export {
+  default as Login,
+  default as Registration,
+  default as ResetPassword,
+  default as ApiErrorWatcher,
+  default as RequestPasswordReset,
+} from {
+  Login: import('./Login'),
+  Registration: import('./Registration'),
+  ResetPassword: import('./ResetPassword'),
+  ApiErrorWatcher: import('./ApiErrorWatcher'),
+  RequestPasswordReset: import('./RequestPasswordReset'),
+};
