581e77cb5d3ad00fab52c6cbcf6f9c9092845c5a
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:10 -0300

    improve

--- client/src/components/Chat/Messages/Content/ContentParts.tsx
+++ client/src/components/Chat/Messages/Content/ContentParts.tsx
@@ -1,50 +1,49 @@
-import { Suspense } from 'react';
-import type { TMessageContentParts } from 'librechat-data-provider';
+import { Suspense, forwardRef } from 'react';
+import type { TMessageContentParts, TMessageProps } from 'librechat-data-provider';
 import { UnfinishedMessage } from './MessageContent';
 import { DelayedRender } from '~/components/ui';
 import Part from './Part';
 
-const ContentParts = ({
-  error,
-  unfinished,
-  isSubmitting,
-  isLast,
-  content,
-  ...props
-}: // eslint-disable-next-line @typescript-eslint/no-explicit-any
-any) => {
-  if (error) {
-    // return <ErrorMessage text={text} />;
+type ContentPartsProps = TMessageProps & {
+  error?: string;
+  unfinished?: boolean;
+  isSubmitting: boolean;
+  isLast: boolean;
+};
+
+const ContentParts = forwardRef<HTMLDivElement, ContentPartsProps>((props, ref) => {
+  if (props.error) {
+    return <ErrorMessage text={props.error} />;
   } else {
     const { message } = props;
     const { messageId } = message;
 
     return (
-      <>
-        {content
-          .filter((part: TMessageContentParts | undefined) => part)
-          .map((part: TMessageContentParts | undefined, idx: number) => {
-            const showCursor = idx === content.length - 1 && isLast;
+      <div ref={ref}>
+        {props.content
+          .filter((part): part is TMessageContentParts => part !== undefined)
+          .map((part, idx) => {
+            const showCursor = idx === props.content.length - 1 && props.isLast;
             return (
               <Part
                 key={`display-${messageId}-${idx}`}
-                showCursor={showCursor && isSubmitting}
-                isSubmitting={isSubmitting}
+                showCursor={showCursor && props.isSubmitting}
+                isSubmitting={props.isSubmitting}
                 part={part}
                 {...props}
               />
             );
           })}
-        {!isSubmitting && unfinished && (
+        {!props.isSubmitting && props.unfinished && (
           <Suspense>
             <DelayedRender delay={250}>
               <UnfinishedMessage key={`unfinished-${messageId}`} />
             </DelayedRender>
           </Suspense>
         )}
-      </>
+      </div>
     );
   }
-};
+});
 
 export default ContentParts;
