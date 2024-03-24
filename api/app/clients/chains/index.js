30728ec75e9b2630259d22e01c8fdd5033399102
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:54:48 -0300

    improve

--- api/app/clients/chains/index.js
+++ api/app/clients/chains/index.js
@@ -1,7 +1,14 @@
 const runTitleChain = require('./runTitleChain');
 const predictNewSummary = require('./predictNewSummary');
 
+// Export the functions using the module.exports object
 module.exports = {
   runTitleChain,
   predictNewSummary,
 };
+
+// Add some error handling to ensure that the required modules are properly defined
+if (typeof runTitleChain !== 'function' || typeof predictNewSummary !== 'function') {
+  throw new Error('One or more required modules are not properly defined');
+}
+
