bc37d0deb2e55f0fbfb1060480c5bdd774d156ca
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:56:17 -0300

    improve

--- client/src/hooks/useLocalize.ts
+++ client/src/hooks/useLocalize.ts
@@ -1,8 +1,2 @@
 import { useRecoilValue } from 'recoil';
 import { localize } from '~/localization/Translation';
-import store from '~/store';
-
-export default function useLocalize() {
-  const lang = useRecoilValue(store.lang);
-  return (phraseKey: string, ...values: string[]) => localize(lang, phraseKey, ...(values ?? []));
-}
