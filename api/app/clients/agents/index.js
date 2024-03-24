5ac2b5732b3d59124190c1477486d53cddc1ea2d
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:20 -0300

    improve

--- api/app/clients/agents/index.js
+++ api/app/clients/agents/index.js
@@ -1,7 +1,5 @@
-const initializeCustomAgent = require('./CustomAgent/initializeCustomAgent');
-const initializeFunctionsAgent = require('./Functions/initializeFunctionsAgent');
-
 module.exports = {
-  initializeCustomAgent,
-  initializeFunctionsAgent,
+  initializeCustomAgent: require('./CustomAgent/initializeCustomAgent'),
+  initializeFunctionsAgent: require('./Functions/initializeFunctionsAgent'),
 };
+
