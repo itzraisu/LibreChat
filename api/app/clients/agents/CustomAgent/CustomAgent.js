7e20452d2fa44d748ad3a321d5925974e0f1f72c
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:54:43 -0300

    improve

--- api/app/clients/agents/CustomAgent/CustomAgent.js
+++ api/app/clients/agents/CustomAgent/CustomAgent.js
@@ -3,43 +3,50 @@ const { PromptTemplate, renderTemplate } = require('langchain/prompts');
 const { gpt3, gpt4 } = require('./instructions');
 
 class CustomAgent extends ZeroShotAgent {
-  constructor(input) {
+  constructor(input, opts = {}) {
     super(input);
+    this.opts = opts;
   }
 
   _stop() {
     return ['\nObservation:', '\nObservation 1:'];
   }
 
-  static createPrompt(tools, opts = {}) {
-    const { currentDateString, model } = opts;
+  static async createPrompt(tools, opts = {}) {
+    const { currentDateString, model = 'gpt-3' } = opts;
     const inputVariables = ['input', 'chat_history', 'agent_scratchpad'];
 
     let prefix, instructions, suffix;
-    if (model.includes('gpt-3')) {
-      prefix = gpt3.prefix;
-      instructions = gpt3.instructions;
-      suffix = gpt3.suffix;
-    } else if (model.includes('gpt-4')) {
-      prefix = gpt4.prefix;
-      instructions = gpt4.instructions;
-      suffix = gpt4.suffix;
+    try {
+      if (model === 'gpt-3') {
+        ({ prefix, instructions, suffix } = gpt3);
+      } else if (model === 'gpt-4') {
+        ({ prefix, instructions, suffix } = gpt4);
+      } else {
+        throw new Error(`Unsupported model: ${model}`);
+      }
+    } catch (error) {
+      console.error(error);
+      return;
     }
 
     const toolStrings = tools
       .filter((tool) => tool.name !== 'self-reflection')
       .map((tool) => `${tool.name}: ${tool.description}`)
       .join('\n');
+
     const toolNames = tools.map((tool) => tool.name);
-    const formatInstructions = (0, renderTemplate)(instructions, 'f-string', {
+    const formatInstructions = renderTemplate(instructions, 'f-string', {
       tool_names: toolNames,
     });
+
     const template = [
       `Date: ${currentDateString}\n${prefix}`,
       toolStrings,
       formatInstructions,
       suffix,
     ].join('\n\n');
+
     return new PromptTemplate({
       template,
       inputVariables,
