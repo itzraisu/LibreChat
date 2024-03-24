4508d1aba033d9b7b0b63880ac2dfa9908201fe7
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:08 -0300

    improve

--- api/server/routes/files/index.js
+++ api/server/routes/files/index.js
@@ -1,27 +1,42 @@
+npm install express-async-errors
+
+
 const express = require('express');
 const createMulterInstance = require('./multer');
 const { uaParser, checkBan, requireJwtAuth, createFileLimiters } = require('~/server/middleware');
-
-const files = require('./files');
-const images = require('./images');
-const avatar = require('./avatar');
+const { files, images, avatar } = require('./routes');
+const { handleFileUpload, handleImageUpload } = require('./handlers');
+const { errorHandler } = require('~/server/error-handling');
 
 const initialize = async () => {
   const router = express.Router();
   router.use(requireJwtAuth);
   router.use(checkBan);
   router.use(uaParser);
+  router.use(errorHandler);
 
-  const upload = await createMulterInstance();
-  const { fileUploadIpLimiter, fileUploadUserLimiter } = createFileLimiters();
+  const upload = await createMulterInstance().catch((err) => {
+    // Handle error here
+    console.error('Error creating Multer instance:', err);
+  });
+
+  const { fileUploadIpLimiter, fileUploadUserLimiter } = createFileLimiters().catch((err) => {
+    // Handle error here
+    console.error('Error creating file limiters:', err);
+  });
+
   router.post('*', fileUploadIpLimiter, fileUploadUserLimiter);
-  router.post('/', upload.single('file'));
-  router.post('/images', upload.single('file'));
+
+  router.post('/', handleFileUpload(upload));
+  router.post('/images', handleImageUpload(upload));
 
   router.use('/', files);
   router.use('/images', images);
   router.use('/images/avatar', avatar);
+
   return router;
 };
 
-module.exports = { initialize };
+const handleFileUpload = (upload) => async (req, res, next) => {
+  try {
+
