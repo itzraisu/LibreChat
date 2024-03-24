a6e976b678358d0f915aa4d9e5cb943ac65975d1
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:13 -0300

    improve

--- e2e/specs/popup.spec.ts
+++ e2e/specs/popup.spec.ts
@@ -2,15 +2,23 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Endpoints Presets suite', () => {
   test('Endpoints Suite', async ({ page }) => {
-    await page.goto('http://localhost:3080/', { timeout: 5000 });
+    const baseURL = 'http://localhost:3080/';
+    const timeout = 5000;
+    const endpointName = 'ChatGPT OpenAI';
+
+    await page.goto(baseURL, { timeout });
+
+    // Click on the new conversation menu
     await page.getByTestId('new-conversation-menu').click();
 
-    // includes the icon + endpoint names in obj property
-    const endpointItem = page.getByRole('menuitemradio', { name: 'ChatGPT OpenAI' });
+    // Click on the desired endpoint item
+    const endpointItem = page.getByRole('menuitemradio', { name: endpointName });
     await endpointItem.click();
 
-    await page.getByTestId('new-conversation-menu').click();
     // Check if the active class is set on the selected endpoint
     expect(await endpointItem.getAttribute('class')).toContain('active');
+
+    // Clean up by clicking on the new conversation menu again
+    await page.getByTestId('new-conversation-menu').click();
   });
 });
