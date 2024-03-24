84f3ac031f6d96574f4af39a1c7914069d529c27
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:16 -0300

    improve

--- client/src/components/Endpoints/Settings/MultiView/Google.tsx
+++ client/src/components/Endpoints/Settings/MultiView/Google.tsx
@@ -2,27 +2,47 @@ import Settings from '../Google';
 import Examples from '../Examples';
 import { useSetOptions } from '~/hooks';
 import { useRecoilValue } from 'recoil';
+import type { Conversation, OptionSettings } from '~/types';
 import store from '~/store';
 
-export default function GoogleView({ conversation, models, isPreset = false }) {
-  const optionSettings = useRecoilValue(store.optionSettings);
-  const { setOption, setExample, addExample, removeExample } = useSetOptions(
-    isPreset ? conversation : null,
-  );
+export default function GoogleView({
+  conversation,
+  models,
+  isPreset = false,
+}: {
+  conversation: Conversation | null;
+  models: unknown[];
+  isPreset?: boolean;
+}) {
+  const optionSettings = useRecoilValue<OptionSettings>(store.optionSettings);
+  const {
+    setOption,
+    setExample,
+    addExample,
+    removeExample,
+  } = useSetOptions(isPreset ? conversation : null);
+
   if (!conversation) {
     return null;
   }
 
-  const { examples } = conversation;
+  const { examples = [] } = conversation;
   const { showExamples, isCodeChat } = optionSettings;
+
   return showExamples && !isCodeChat ? (
     <Examples
-      examples={examples ?? []}
+      key={conversation.id}
+      examples={examples}
       setExample={setExample}
       addExample={addExample}
       removeExample={removeExample}
     />
   ) : (
-    <Settings conversation={conversation} setOption={setOption} models={models} />
+    <Settings
+      conversation={conversation}
+      setOption={setOption}
+      models={models}
+    />
   );
 }
+
