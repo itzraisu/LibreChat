15aec1bbef632d1a277519d1ff90a1b10e3af92c
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:16 -0300

    improve

--- api/server/routes/plugins.js
+++ api/server/routes/plugins.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { getAvailablePluginsController } = require('../controllers/PluginController');
+const PluginController = require('../controllers/PluginController');
 const requireJwtAuth = require('../middleware/requireJwtAuth');
 
 const router = express.Router();
 
-router.get('/', requireJwtAuth, getAvailablePluginsController);
+// Get all available plugins
+router.get('/', requireJwtAuth, PluginController.getAvailablePlugins);
 
+// Add more routes here as needed
+// ...
+
+// Export the router
 module.exports = router;
+
