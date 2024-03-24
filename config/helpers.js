4c4b19c7d3586d6f470eafcc92594b200ac3718e
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:56:08 -0300

    improve

--- config/helpers.js
+++ config/helpers.js
@@ -8,33 +8,52 @@ const readline = require('readline');
 const { execSync } = require('child_process');
 
 const askQuestion = (query) => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  return new Promise((resolve) =>
     rl.question('\x1b[36m' + query + '\n> ' + '\x1b[0m', (ans) => {
       rl.close();
       resolve(ans);
-    }),
-  );
+    });
+  }).catch((err) => {
+    console.error(err);
+  });
 };
 
 function isDockerRunning() {
-  try {
-    execSync('docker info');
-    return true;
-  } catch (e) {
-    return false;
+  if (process.platform === 'win32') {
+    try {
+      execSync('cmd /c ver');
+      return true;
+    } catch (e) {
+      return false;
+    }
+  } else {
+    try {
+      execSync('docker info');
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
 
 function deleteNodeModules(dir) {
+  if (typeof dir !== 'string') {
+    throw new Error('dir must be a string');
+  }
+
   const nodeModulesPath = path.join(dir, 'node_modules');
   if (fs.existsSync(nodeModulesPath)) {
     console.purple(`Deleting node_modules in ${dir}`);
-    fs.rmSync(nodeModulesPath, { recursive: true });
+    fs.rmdir(nodeModulesPath, { recursive: true }, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
   }
 }
 
@@ -46,17 +65,4 @@ const silentExit = (code = 0) => {
 // Set the console colours
 console.orange = (msg) => console.log('\x1b[33m%s\x1b[0m', msg);
 console.green = (msg) => console.log('\x1b[32m%s\x1b[0m', msg);
-console.red = (msg) => console.log('\x1b[31m%s\x1b[0m', msg);
-console.blue = (msg) => console.log('\x1b[34m%s\x1b[0m', msg);
-console.purple = (msg) => console.log('\x1b[35m%s\x1b[0m', msg);
-console.cyan = (msg) => console.log('\x1b[36m%s\x1b[0m', msg);
-console.yellow = (msg) => console.log('\x1b[33m%s\x1b[0m', msg);
-console.white = (msg) => console.log('\x1b[37m%s\x1b[0m', msg);
-console.gray = (msg) => console.log('\x1b[90m%s\x1b[0m', msg);
-
-module.exports = {
-  askQuestion,
-  silentExit,
-  isDockerRunning,
-  deleteNodeModules,
-};
+console.red = (msg) => console.log
