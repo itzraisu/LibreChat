d49aec23da3d8e8b99b163592ab8f4d9dca8d29e
itzraisu <133676178+itzraisu@users.noreply.github.com>
2024-03-24 18:55:23 -0300

    improve

--- client/src/hooks/Messages/index.ts
+++ client/src/hooks/Messages/index.ts
@@ -1,4 +1,16 @@
-export { default as useAvatar } from './useAvatar';
-export { default as useProgress } from './useProgress';
-export { default as useMessageHelpers } from './useMessageHelpers';
-export { default as useMessageScrolling } from './useMessageScrolling';
+const messageFunctions = {
+  useAvatar: () => import('./useAvatar').then(module => module.default),
+  useProgress: () => import('./useProgress').then(module => module.default),
+  useMessageHelpers: () => import('./useMessageHelpers').then(module => module.default),
+  useMessageScrolling: () => import('./useMessageScrolling').then(module => module.default),
+};
+
+export default messageFunctions;
+
+
+import messageFunctions from './messageFunctions';
+
+const { useAvatar, useProgress, useMessageHelpers, useMessageScrolling } = messageFunctions;
+
+
+import { useAvatar, useProgress } from './messageFunctions';
